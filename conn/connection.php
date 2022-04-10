54676d4cac8a8d95b764a09d003db765255652ee
BanthonyE <69830664+BanthonyE@users.noreply.github.com>
2022-04-10 00:17:27 -0500

    configuracion bd

--- conn/connection.php
+++ conn/connection.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$conn = new PDO('mysql:host=localhost; dbname=bdcalificaciones', 'root', '');
+$conn = new PDO('mysql:host=localhost; dbname=heroku_773c0186875e0b3', 'b080efe90b1bbb', '66b8be8b');
 } catch(PDOException $e){
    echo "Error: ". $e->getMessage();
    die();
