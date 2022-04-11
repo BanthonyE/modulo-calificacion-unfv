cce675836c790775f8e1f4056394756d2b4115d4
BanthonyE <69830664+BanthonyE@users.noreply.github.com>
2022-04-10 21:11:35 -0500

    aaa

---
+++ admin_delete_asignacion.php
@@ -0,0 +1,20 @@
+<?php
+require 'functions.php';
+if($_SESSION['rol'] =='Administrador') {
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        try {
+            $id_docasig = $_GET['id'];
+            
+            $users = $conn->prepare("delete from docenteasignatura where id_docasig = " . $id_docasig);
+            $users->execute();
+            header('location:admin_listado_asignacion.php');
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    } else {
+        die('Ha ocurrido un error');
+    }
+}else{
+    header('location:inicio.view.php?err=1');
+}
+?>
